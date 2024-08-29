bd786594bbcd1b2f5c48ec73dc59cf4fd0c96c57
Samuel-Mark <samuel.mark@sky.uk>
2024-08-29 16:50:55 +0100

    Create Error Pages

--- application/routes.py
+++ application/routes.py
@@ -12,7 +12,20 @@ def home():
 def login():
     return render_template('login.html')
 
-
 @app.route('/menu')
 def menu():
     return render_template('menu.html', title='Check the Menu')
+
+# Error handling routes
+@app.errorhandler(404)
+def page_not_found(e):
+    error = 404
+    return ErrorPage(error)
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    error = 500
+    return ErrorPage(error)
+
+def ErrorPage(error):
+    return render_template('error.html', title='Oopsie...', error_type=error), error
