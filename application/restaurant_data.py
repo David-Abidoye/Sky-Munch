7fbf3dbc0b992f90786b5b3ebe3bb61b45b78de7
David Abidoye <akinloluabidoye.aa@gmail.com>
2024-09-02 12:59:36 +0100

    EDIT: added the functionality of restaurant search to the search area

---
+++ application/restaurant_data.py
@@ -0,0 +1,6 @@
+restaurants = [
+    {"id": 1, "name": "The Pavilion", "location": "Downtown"},
+    {"id": 2, "name": "The Dining Room", "location": "Uptown"},
+    {"id": 3, "name": "The Garden", "location": "Midtown"},
+    {"id": 4, "name": "The Restaurant", "location": "West Side"},
+]
